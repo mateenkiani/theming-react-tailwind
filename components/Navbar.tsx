6eda901bc85ad43e029f689a2fcc98a43abccbd9
mateenkiani <kiani.mateen012@gmail.com>
2023-03-04 14:37:58 +0500

    navbar fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
       <div className="w-full backdrop-blur-sm ">
         <div className="relative z-1 h-16 mx-auto px-5 max-w-7xl flex items-center justify-between">
           <a className="text-2xl hover:text-cyan-400 transition-colors"  href="">
-            Logo
+            Milddev
           </a>
 
           <ul className="flex items-center gap-5 ">
